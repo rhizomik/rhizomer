f4a97992e729c728b99c16030043cb182eed9983
Roberto García <rogargon@gmail.com>
2013-01-27 01:07:44 +0100

    Dealing with path to users file, a solution independent from delivery path is still required

--- rhizomer-foafssl/src/main/java/net/rhizomik/rhizomer/authentication/Constants/Constants.java
+++ rhizomer-foafssl/src/main/java/net/rhizomik/rhizomer/authentication/Constants/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
     //Tipo de encriptació que s'utilitza per la contrasenya
     public static final String TIPO_ENCRIPTACIO = "MD5";
     //Ruta on es troba el fitxer si s'executem la clase de CreacioUsuaris!
-    public static final String RUTA_FITXER_CREACIO_USUARIS = "web\\WEB-INF\\"+NOM_FITXER_USUARIS;
+    public static final String RUTA_FITXER_CREACIO_USUARIS = "rhizomer-foafssl\\src\\main\\webapp\\WEB-INF\\"+NOM_FITXER_USUARIS;
 
     public static final String NOM_SESSIO_USUARI= "net.rhizomik.rhizomer.authentication";
 }

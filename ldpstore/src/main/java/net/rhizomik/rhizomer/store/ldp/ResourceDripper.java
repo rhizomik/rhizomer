6c694d4cafc8eb5fc0eee001d666d059771f8dd3
davidkaste <d.castella.85@gmail.com>
2014-11-18 12:29:13 +0100

    Refactored ResourceDripper.java

--- ldpstore/src/main/java/net/rhizomik/rhizomer/store/ldp/ResourceDripper.java
+++ ldpstore/src/main/java/net/rhizomik/rhizomer/store/ldp/ResourceDripper.java
@@ -1,37 +1,83 @@
 package net.rhizomik.rhizomer.store.ldp;
 
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.ontology.OntResource;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
- * Created by davidkaste on 04/11/14.
+ * Created by David Castellà david.castella@udl.cat on 04/11/14.
+ *
  */
 public class ResourceDripper {
-    private Model model = ModelFactory.createDefaultModel();
+    private OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
 
-    public ResourceDripper (/*String file*/) {
-        model.read("file:///home/davidkaste/Documents/administracions-export-20140922-100143.rdf", "RDF/XML");
+    public ResourceDripper (String fileUri) {
+        model.read(fileUri, "RDF/XML");
     }
 
-    public void importResources2LDP (URL serverURI, String containerName) {
-        model.write(System.out, "RDF/XML");
-        /*String queryString = "...";
-        Query query = QueryFactory.create(queryString);
-        QueryExecution qexec = QueryExecutionFactory.create(query, model);
-        Model resultModel = qexec.execDescribe();
-        resultModel.write(System.out);*/
+    private String describeURI(OntResource resource) {
+        String sparql;
+        Query q;
+        QueryExecution qe;
+        Model resultModel;
+        StringWriter out;
+
+        sparql = "DESCRIBE <" + resource.getURI() + ">";
+        q = QueryFactory.create(sparql);
+        qe = QueryExecutionFactory.create(q, resource.getModel());
+        resultModel = qe.execDescribe();
+        out = new StringWriter();
+        resultModel.write(out, "RDF/XML");
+
+        return out.toString();
+    }
+
+    private void simpleImport2LDP(String resourceUri, String resourceRDF, URL serverUri, String containerName) {
+        WebResource wr;
+        Client c = Client.create();
+        ClientResponse cr;
+
+        wr = c.resource(serverUri.toString() + "/" + containerName + "/");
+        String identifier = resourceUri.split("/")[resourceUri.split("/").length - 1];
+        cr = wr.type("application/rdf+xml")
+                .header("Slug", identifier)
+                .post(ClientResponse.class, resourceRDF);
+    }
+
+    public void importResources2LDP (URL serverUri, String containerName, String classUri) {
+        final OntClass governmentOrganization;
+        String resourceRDF;
+        try {
+            governmentOrganization = model.getOntClass(classUri);
+            for ( final ExtendedIterator<? extends OntResource> gvs = governmentOrganization.listInstances(); gvs.hasNext(); ) {
+                OntResource or = gvs.next();
+                resourceRDF = describeURI(or);
+                simpleImport2LDP(or.getURI(), resourceRDF, serverUri, containerName);
+            }
+        } catch (NullPointerException npe) {
+            System.out.println("There's no class with uri: <" + classUri + ">");
+        }
     }
 
     public static void main(String[] args) throws MalformedURLException {
-        ResourceDripper rd = new ResourceDripper();
-        URL url = new URL("http://localhost:8080/marmotta/ldp/");
-        rd.importResources2LDP(url, "c1");
+        ResourceDripper rd = new ResourceDripper("file:///home/davidkaste/Documents/administracions-old.rdf");
+        URL url = new URL("http://localhost:8080/marmotta/ldp");
+        rd.importResources2LDP(url, "administracions", "http://schema.org/GovernmentOrganization");
     }
 }

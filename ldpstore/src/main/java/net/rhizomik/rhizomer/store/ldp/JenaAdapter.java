612e7d3879631d28457cee7b03c81142dafb22e3
davidkaste <david.castella@riseup.net>
2015-02-10 12:43:54 +0100

    Finalized adapter class to query Marmotta server and return Jena ResultSet.

---
+++ ldpstore/src/main/java/net/rhizomik/rhizomer/store/ldp/JenaAdapter.java
@@ -0,0 +1,26 @@
+package net.rhizomik.rhizomer.store.ldp;
+
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFactory;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import org.apache.http.client.utils.URIBuilder;
+
+import java.io.ByteArrayInputStream;
+
+/**
+ * Created by davidkaste on 04/02/15.
+ * Gets Apache Marmotta Objects and returns Apache Jena Objects
+ */
+public abstract class JenaAdapter {
+    public static ResultSet querySelectMarmotta(String query, String SPARQLEndpoint) {
+        URIBuilder ub = new URIBuilder();
+        Client c = Client.create();
+        WebResource wr = c.resource(SPARQLEndpoint + "select" + ub.addParameter("query", query).addParameter("output", "application/json"));
+        wr.toString();
+        String cr = wr.get(String.class);
+        System.out.println(cr);
+        ResultSet ret = ResultSetFactory.fromJSON(new ByteArrayInputStream(cr.getBytes()));
+        return ret;
+    }
+}
